e7ff5216bfdf180a81957827bda08c56cf7a557b
Astrid Ge <qiutongg@umich.edu>
2024-10-30 19:31:09 -0700

    complete add function

--- src/App.jsx
+++ src/App.jsx
@@ -77,23 +77,49 @@ const App = () => {
       img: 'https://via.placeholder.com/150/602b9e',
     },
   ])
-  console.log(zombieFighters)
-  console.log(money)
-  console.log(team)
+  const [strength, setStrength] = useState(0);
+  const [agility, setAglity] = useState(0);
+
+  const handleAddFighter = (newFighter) => {
+    console.log(newFighter)
+    const newTeam = [...team, newFighter]
+    console.log(newTeam)
+    setTeam(newTeam)
+  }
 
   return (
     <>
     <h1>Hello world!</h1>
-    <h3>Fighters</h3>
-    <div class="fighterInfo">
-        {zombieFighters.map((eachFighter, idx) => (
-          <ul>
+    <h3>Money: {money}</h3>
+    <h3>Strength: {strength}</h3>
+    <h3>Agility: {agility}</h3>
+    <h3>Team: </h3>
+    <ul>
+      {team.length > 0 ? (
+        team.map((eachFighter, idx) => (
+          <li key={idx}> 
             <img src= {eachFighter.img}></img>
-            <li key={idx}> {eachFighter.name} </li>
-            <li key={idx}> Price: {eachFighter.price} </li>
-            <li key={idx}> Strength: {eachFighter.strength} </li>
-            <li key={idx}> Agility: {eachFighter.agility} </li>
-            <button> Add </button>
+            <p> {eachFighter.name} </p>
+            <p> Price: {eachFighter.price} </p>
+            <p> Strength: {eachFighter.strength} </p>
+            <p> Agility: {eachFighter.agility} </p>
+          </li>
+        ))
+      ) : (
+        <li> pick some team members</li>
+      )}
+    </ul>
+
+    <h3>Fighters</h3>
+    <div className="fighterInfo">
+        {zombieFighters.map((eachFighter, idx) => (
+          <ul key={idx}>
+            <img src= {eachFighter.img}></img>
+            <li> {eachFighter.name} </li>
+            <li> Price: {eachFighter.price} </li>
+            <li> Strength: {eachFighter.strength} </li>
+            <li> Agility: {eachFighter.agility} </li>
+            <button onClick= {()=> handleAddFighter(eachFighter)}> Add </button>
           </ul>
         ))}
     </div>
